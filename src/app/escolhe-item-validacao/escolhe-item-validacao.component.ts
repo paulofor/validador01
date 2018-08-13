fd8606749154fe758598d7134c5e45359ba93a63
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2018-08-13 13:48:16 -0300

    Associando ItemValidacao com Pagina

--- src/app/escolhe-item-validacao/escolhe-item-validacao.component.ts
+++ src/app/escolhe-item-validacao/escolhe-item-validacao.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { EscolhedorItemVallidacaoComponent } from '../escolhedor-item-vallidacao/escolhedor-item-vallidacao.component';
-import { ProjetoCanvasMySql, ProjetoMySqlApi } from '../shared/sdk';
+import { ProjetoCanvasMySql, ProjetoMySqlApi, PaginaValidacaoWebApi } from '../shared/sdk';
+import { ParamMap } from '@angular/router';
+import { PaginaValidacaoWeb } from '../shared/sdk/models';
+import { Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-escolhe-item-validacao',
@@ -12,25 +17,33 @@ import { ProjetoCanvasMySql, ProjetoMySqlApi } from '../shared/sdk';
 export class EscolheItemValidacaoComponent implements OnInit {
 
 
-
+  consulta = {"include" : ["projeto","itemValidacaoPaginas"] };
 
 
   @Input() pagina;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private route: ActivatedRoute, private dialog: MatDialog, private srvPagina: PaginaValidacaoWebApi, private router: Router ) { }
 
   ngOnInit() {
 
   }
 
+  atualizaPagina() {
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.srvPagina.findById(this.pagina.id, this.consulta)
+      )).subscribe((paginaResult: PaginaValidacaoWeb) => {
+        this.pagina = paginaResult;
+      })
+  }
  
 
 
   openDialog(pagina) {
     console.log('Pagina:', pagina);
     this.dialog.afterAllClosed.subscribe(result => {
-      console.log(`Dialog result: ${result}`); 
-      //this.carregaDados();
+      console.log('Dialog result: ${result}'); 
+      this.atualizaPagina();
     });
     this.dialog.open(EscolhedorItemVallidacaoComponent, {
       width: '800px',
--- src/app/escolhedor-item-vallidacao/escolhedor-item-vallidacao.component.ts
+++ src/app/escolhedor-item-vallidacao/escolhedor-item-vallidacao.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { ProjetoMySqlApi, ProjetoCanvasMySql, ItemValidacaoPagina } from '../shared/sdk';
-import {DropEvent} from 'ng-drag-drop';
+import { ProjetoMySqlApi, ProjetoCanvasMySql, ItemValidacaoPagina, PaginaValidacaoWeb, ItemValidacaoPaginaApi } from '../shared/sdk';
+import { DropEvent } from 'ng-drag-drop';
 
 @Component({
   selector: 'app-escolhedor-item-vallidacao',
@@ -15,9 +15,11 @@ export class EscolhedorItemVallidacaoComponent implements OnInit {
   listaItem: ItemValidacaoPagina[] = new Array(0);
 
   droppedItems: ItemValidacaoPagina[] = new Array(0);
+  pagina: PaginaValidacaoWeb;
 
   constructor(public dialogRef: MatDialogRef<EscolhedorItemVallidacaoComponent>
-    , @Inject(MAT_DIALOG_DATA) public data: any, private srv: ProjetoMySqlApi) {
+    , @Inject(MAT_DIALOG_DATA) public data: any, private srv: ProjetoMySqlApi,
+    private itemValidacaoSrv: ItemValidacaoPaginaApi) {
   }
 
   ngOnInit() {
@@ -25,7 +27,8 @@ export class EscolhedorItemVallidacaoComponent implements OnInit {
   }
 
   carregaListaValor() {
-    this.srv.getProjetoCanvasMySqls(this.data.pagina.projeto.id, this.consulta)
+    this.pagina = this.data.pagina;
+    this.srv.getProjetoCanvasMySqls(this.pagina.projeto.id, this.consulta)
       .subscribe((result: ProjetoCanvasMySql[]) => {
         this.listaValor = result;
 
@@ -47,8 +50,19 @@ export class EscolhedorItemVallidacaoComponent implements OnInit {
 
 
   onDrop(e: DropEvent) {
-    this.droppedItems.push(e.dragData);
-    this.removeItem(e.dragData, this.listaItem)
+    console.log('onDrop:', e.dragData);
+    let item = <ItemValidacaoPagina>e.dragData;
+    console.log('Data:', this.data);
+    item.paginaValidacaoWebId = this.pagina.id;
+    console.log('Pagina:', this.pagina);
+    console.log('Item', item);
+    this.itemValidacaoSrv.updateAttributes(item.id, item)
+      .subscribe((result: any) => {
+        console.log('Resultado: ', result);
+        this.droppedItems.push(e.dragData);
+        this.removeItem(e.dragData, this.listaItem)
+      })
+
   }
 
   removeItem(item: any, list: Array<any>) {

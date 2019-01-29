418cc778cede832401b8d96dc06048535bfae7d0
Paulo Alexandre <paulofore@gmail.com>
2019-01-29 21:19:12 -0200

    planejamento

--- src/app/plano-semana-por-contexto/plano-semana-por-contexto.component.ts
+++ src/app/plano-semana-por-contexto/plano-semana-por-contexto.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DiaSemana, ProcessoNegocio, Semana, ProcessoNegocioApi, PlanoExecucao } from '../shared/sdk';
 import { Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-plano-semana-por-contexto',
@@ -9,12 +10,12 @@ import { Input } from '@angular/core';
 })
 export class PlanoSemanaPorContextoComponent implements OnInit {
 
-  listaDiaSemana : DiaSemana[];
-  processoComPlano : ProcessoNegocio[];
+  listaDiaSemana: DiaSemana[];
+  processoComPlano: ProcessoNegocio[];
 
-  @Input() contexto : string;
-  @Input() semana : Semana;
-  
+  @Input() contexto: string;
+  @Input() semana: Semana;
+  myControl = new FormControl();
 
   constructor(private srv: ProcessoNegocioApi) { }
 
@@ -23,22 +24,26 @@ export class PlanoSemanaPorContextoComponent implements OnInit {
   }
 
   carregaProcesso() {
-    this.srv.ObtemComPlanoPorSemana(this.semana.id,1)
+    this.srv.ObtemComPlanoPorSemana(this.semana.id, 1)
       .subscribe((resultado) => {
         this.processoComPlano = resultado;
-        this.processoComPlano.map((processo:ProcessoNegocio)=> {
-          if (processo.planoExecucaos.length==0) {
-            processo.planoExecucaos.push(new PlanoExecucao()); // segunda
-            processo.planoExecucaos.push(new PlanoExecucao()); // terca
-            processo.planoExecucaos.push(new PlanoExecucao()); // quarta
-            processo.planoExecucaos.push(new PlanoExecucao()); // quinta
-            processo.planoExecucaos.push(new PlanoExecucao()); // sexta
-            processo.planoExecucaos.push(new PlanoExecucao()); // sabado
-            processo.planoExecucaos.push(new PlanoExecucao()); // domingo
+        this.processoComPlano.map((processo: ProcessoNegocio) => {
+          if (processo.planoExecucaos.length == 0) {
+            processo.planoExecucaos.push(new PlanoExecucao({ "processoNegocioId": processo.id, "tempoEstimado": new Date(0), "diaSemanaId": 1 })); // segunda
+            processo.planoExecucaos.push(new PlanoExecucao({ "processoNegocioId": processo.id, "tempoEstimado": new Date(0), "diaSemanaId": 2 })); // terca
+            processo.planoExecucaos.push(new PlanoExecucao({ "processoNegocioId": processo.id, "tempoEstimado": new Date(0), "diaSemanaId": 3 })); // quarta
+            processo.planoExecucaos.push(new PlanoExecucao({ "processoNegocioId": processo.id, "tempoEstimado": new Date(0), "diaSemanaId": 4 })); // quinta
+            processo.planoExecucaos.push(new PlanoExecucao({ "processoNegocioId": processo.id, "tempoEstimado": new Date(0), "diaSemanaId": 5 })); // sexta
+            processo.planoExecucaos.push(new PlanoExecucao({ "processoNegocioId": processo.id, "tempoEstimado": new Date(0), "diaSemanaId": 6 })); // sabado
+            processo.planoExecucaos.push(new PlanoExecucao({ "processoNegocioId": processo.id, "tempoEstimado": new Date(0), "diaSemanaId": 7 })); // domingo
 
           }
         })
       });
+  }
+
+  onSubmit() {
+    console.log("Lista:" , this.processoComPlano);
   }
 
 }

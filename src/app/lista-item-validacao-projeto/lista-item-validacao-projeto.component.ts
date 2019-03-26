64e16c0092e46d3cfba0fd818a68a84511160462
Paulo Alexandre <paulofore@gmail.com>
2019-03-25 21:09:18 -0300

    telaApp

--- src/app/lista-item-validacao-projeto/lista-item-validacao-projeto.component.ts
+++ src/app/lista-item-validacao-projeto/lista-item-validacao-projeto.component.ts
@@ -3,6 +3,7 @@ import { ItemValidacaoPagina, ConceitoProduto, ProjetoMySql, ProjetoMySqlApi } f
 import { MatDialog } from '@angular/material';
 import { ActivatedRoute, Params } from '@angular/router';
 import { EditaCriaItemValidacaoProjetoComponent } from '../edita-cria-item-validacao-projeto/edita-cria-item-validacao-projeto.component';
+import { GerenciaTelaAppComponent } from '../gerencia-tela-app/gerencia-tela-app.component';
 
 @Component({
   selector: 'app-lista-item-validacao-projeto',
@@ -44,6 +45,21 @@ export class ListaItemValidacaoProjetoComponent implements OnInit {
       this.carregaItemValidacao();
     });
     this.dialog.open(EditaCriaItemValidacaoProjetoComponent, {
+      width: '800px',
+      data: {
+        item: item,
+        conceito : this.conceito,
+        projeto : this.projeto
+      }
+    });
+  }
+
+  openTelaAppDialog(item?) {
+    this.dialog.afterAllClosed.subscribe(result => {
+      console.log('Dialog result: ${result}');
+      this.carregaItemValidacao();
+    });
+    this.dialog.open(GerenciaTelaAppComponent, {
       width: '800px',
       data: {
         item: item,

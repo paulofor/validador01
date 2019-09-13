d956b19a002fc50231ef2be04689c13d95eb3a9a
Paulo Alexandre <paulofore@gmail.com>
2019-09-12 22:06:38 -0300

    ideia de produto melhorando

--- src/app/display-ideia-melhoria-origem/display-ideia-melhoria-origem.component.ts
+++ src/app/display-ideia-melhoria-origem/display-ideia-melhoria-origem.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IdeiaMelhoria, IdeiaMelhoriaApi, VersaoAppApi, CampanhaAds } from '../shared/sdk';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-display-ideia-melhoria-origem',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayIdeiaMelhoriaOrigemComponent implements OnInit {
 
-  constructor() { }
+  @Input() campanha: CampanhaAds;
+  //versaoApp: VersaoApp;
+
+  ideiaOrigem : IdeiaMelhoria;
+
+  constructor(private srv: IdeiaMelhoriaApi, private dialog:MatDialog, private srvVersaoApp : VersaoAppApi) { }
 
   ngOnInit() {
+    if (this.campanha) {
+      console.log('IdeiaMelhoriaCampanhaOrigemComponent-Existe campanha (id:' + this.campanha.id + ')');
+      this.carregaIdeia();
+    } 
+    //if (this.campanha.versaoAppId && this.campanha.versaoAppId!=0) {
+    //console.log('IdeiaMelhoriaCampanhaOrigemComponent-Existe versaoApp');
+    //  this.carregaVersaoApp();
+    //}
   }
+
+  carregaIdeia() {
+    let filtro = {"where" : {"campanhaAdsOrigemId" : this.campanha.id }};
+    console.log('filtro:' , filtro);
+    this.srv.findById(this.campanha.ideiaMelhoriaId)
+      .subscribe((result:IdeiaMelhoria) => {
+        console.log('IdeiaMelhoriaCampanhaOrigemComponent-ideiaOrigem', result);
+        this.ideiaOrigem = result;
+      })
+  }
+
+  /*
+  carregaVersaoApp() {
+    this.srvVersaoApp.findById(this.campanha.versaoAppId)
+      .subscribe((result:VersaoApp) => {
+        console.log('IdeiaMelhoriaCampanhaOrigemComponent-Resultado', result);
+        this.versaoApp = result;
+      })
+  }
+  */
 
 }
--- src/app/ideia-melhoria-campanha-origem/ideia-melhoria-campanha-origem.component.ts
+++ src/app/ideia-melhoria-campanha-origem/ideia-melhoria-campanha-origem.component.ts
@@ -63,4 +63,6 @@ export class IdeiaMelhoriaCampanhaOrigemComponent implements OnInit {
       }
     });
   }
+
+ 
 }

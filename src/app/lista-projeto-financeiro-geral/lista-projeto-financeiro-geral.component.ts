d071bbbac22d697ccd4d656d5684e06a1a00953a
tr626987 <tr626987@oi.corp.net>
2019-10-02 13:02:53 -0300

    financeiro totaliza

--- src/app/lista-projeto-financeiro-geral/lista-projeto-financeiro-geral.component.ts
+++ src/app/lista-projeto-financeiro-geral/lista-projeto-financeiro-geral.component.ts
@@ -11,6 +11,9 @@ export class ListaProjetoFinanceiroGeralComponent implements OnInit {
   projetos: ProjetoMySql[];
   errMess: string;
 
+  total: ProjetoMySql = new ProjetoMySql();
+  valorTotal: number;
+
   constructor(private projetoService: ProjetoMySqlApi) {
 
   }
@@ -18,8 +21,30 @@ export class ListaProjetoFinanceiroGeralComponent implements OnInit {
   ngOnInit() {
     this.projetoService.find({'order' : 'custoCampanha desc'})
       .subscribe((projetos: ProjetoMySql[]) =>
-        this.projetos = projetos
+        this.calculaTotal(projetos)
       );
   }
 
+  calculaTotal(projetos) {
+    this.projetos = projetos
+    this.total = new ProjetoMySql();
+    console.log('Total:' , this.total);
+    this.total.custoCampanha = 0;
+    //projetos.forEach(function (projeto) {
+    //  if (projeto.custoCampanha)
+    //    total.custoCampanha += projeto.custoCampanha;
+    //}); 
+    this.calculaValorTotal();
+  }
+
+  calculaValorTotal() {
+    this.valorTotal = 0;
+    for (var i=0; i < this.projetos.length; i++) {
+      var item = this.projetos[i];
+      this.valorTotal += item.custoCampanha;
+    }
+    console.log('Total:' , this.valorTotal);
+  }
+
+  
 }
